16851f8896f85b9c1b0071f94ca3ca9f65cff022
sergio.olivos.c <sergio.olivos.c@gmail.com>
2013-06-08 05:30:10 +0000

    Requerimientos Pymes

--- src/mx/com/vgati/ccmx/vinculacion/pymes/service/imp/PyMEsServiceImp.java
+++ src/mx/com/vgati/ccmx/vinculacion/pymes/service/imp/PyMEsServiceImp.java
@@ -257,10 +257,10 @@ public class PyMEsServiceImp extends AbstractBaseService implements
 	@Override
 	public List<Requerimientos> getRequerimiento(String busqueda,
 			String tractoraReq, java.sql.Date fechaDesde,
-			java.sql.Date fechaHasta) throws RequerimientosNoObtenidosException {
+			java.sql.Date fechaHasta,int idUsuario) throws RequerimientosNoObtenidosException {
 		try {
 			return pyMEsDao.getRequerimientos(busqueda, tractoraReq,
-					fechaDesde, fechaHasta);
+					fechaDesde, fechaHasta,idUsuario);
 		} catch (DaoException e) {
 			throw new RequerimientosNoObtenidosException(new ExceptionMessage(
 					"Ocurrio un error al consultar los Requerimientos."), e);
