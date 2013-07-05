fe8bb3fc92813977a8e3559baad3674456fc7df1
getsemani.correa <getsemani.correa@gmail.com>
2013-07-05 02:13:20 +0000

    actulizaciones coordinacion de diplomados y consultorias

--- rsc/db/cat_diplomados_ccmx.sql
+++ rsc/db/cat_diplomados_ccmx.sql
@@ -1,87 +1,83 @@
 
---Cultura Organizacional
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Cultura Organizacional', 1, 'BIMBO', '2013-02-06', '' ), 
-( 'Cultura Organizacional', 1, 'FEMSA', '2013-02-27', '' ), 
-( 'Cultura Organizacional', 1, 'KCM', '2013-03-20', '' );
+-- DIPLOMADOS
+INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, YEAR ) VALUES 
+( 'Cultura Organizacional', 1, 2013 ),
+( 'Estrategia, Planeación e Innovación', 1, 2013 ),
+( 'Manufactura Esbelta', 1, 2013 ),
+( 'Estrategia Comercial, Imágen y Cadena de Distribución', 1, 2013 );
 
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Cultura Organizacional', 2, 'BIMBO', '2013-04-09', '' ), 
-( 'Cultura Organizacional', 2, 'FEMSA', '2013-05-07', '' ), 
-( 'Cultura Organizacional', 2, 'KCM', '2013-06-04', '' );
+INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, YEAR ) VALUES 
+( 'Cultura Organizacional', 2, 2013 ),
+( 'Estrategia, Planeación e Innovación', 2, 2013 ),
+( 'Manufactura Esbelta', 2, 2013 ),
+( 'Estrategia Comercial, Imágen y Cadena de Distribución', 2, 2013 );
 
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Cultura Organizacional', 3, 'BIMBO', '2013-07-02', '' ), 
-( 'Cultura Organizacional', 3, 'FEMSA', '2013-07-31', '' ), 
-( 'Cultura Organizacional', 3, 'KCM', '2013-08-20', '' );
+INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, YEAR ) VALUES 
+( 'Cultura Organizacional', 3, 2013 ),
+( 'Estrategia, Planeación e Innovación', 3, 2013 ),
+( 'Manufactura Esbelta', 3, 2013 ),
+( 'Estrategia Comercial, Imágen y Cadena de Distribución', 3, 2013 );
 
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Cultura Organizacional', 4, 'BIMBO', '2013-09-04', '' ), 
-( 'Cultura Organizacional', 4, 'FEMSA', '2013-10-01', '' ), 
-( 'Cultura Organizacional', 4, 'KCM', '2013-10-20', '' );
-
-
---Estrategia, Planeación e Innovación
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Estrategia, Planeación e Innovación', 1, 'CEMEX', '2013-02-07', '' ), 
-( 'Estrategia, Planeación e Innovación', 1, 'SLR', '2013-03-06', '' ), 
-( 'Estrategia, Planeación e Innovación', 1, 'PROEZA', '2013-04-10', '' );
-
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Estrategia, Planeación e Innovación', 2, 'CEMEX', '2013-04-23', '' ), 
-( 'Estrategia, Planeación e Innovación', 2, 'SLR', '2013-05-14', '' ), 
-( 'Estrategia, Planeación e Innovación', 2, 'PROEZA', '2013-06-11', '' );
-
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Estrategia, Planeación e Innovación', 3, 'CEMEX', '2013-07-09', '' ), 
-( 'Estrategia, Planeación e Innovación', 3, 'SLR', '2013-08-08', '' ), 
-( 'Estrategia, Planeación e Innovación', 3, 'PROEZA', '2013-08-27', '' );
-
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Estrategia, Planeación e Innovación', 4, 'CEMEX', '2013-09-10', '' ), 
-( 'Estrategia, Planeación e Innovación', 4, 'SLR', '2013-10-08', '' ), 
-( 'Estrategia, Planeación e Innovación', 4, 'PROEZA', '2013-11-05', '' );
+INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, YEAR ) VALUES 
+( 'Cultura Organizacional', 4, 2013 ),
+( 'Estrategia, Planeación e Innovación', 4, 2013 ),
+( 'Manufactura Esbelta', 4, 2013 ),
+( 'Estrategia Comercial, Imágen y Cadena de Distribución', 4, 2013 );
 
 
---Manufactura esbelta
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Manufactura Esbelta', 1, 'DEACERO', '2013-02-14', '' ), 
-( 'Manufactura Esbelta', 1, 'MABE', '2013-03-13', '' ), 
-( 'Manufactura Esbelta', 1, 'XIGNUX', '2013-04-02', '' );
+-- SESIONES
+INSERT INTO INFRA.SESIONES ( ID_DIPLOMADO, SESION, EXPOSITOR,FECHA ) VALUES 
+( 1, 1, 'BIMBO', '2013-02-06' ),
+( 1, 2, 'FEMSA', '2013-02-27' ),
+( 1, 3, 'KCM', '2013-03-20' ),
+( 2, 1, 'CEMEX', '2013-02-07' ),
+( 2, 2, 'SLR', '2013-03-06' ),
+( 2, 3, 'PROEZA', '2013-04-10' ),
+( 3, 1, 'DEACERO', '2013-02-14' ),
+( 3, 2, 'MABE', '2013-03-13' ),
+( 3, 3, 'XIGNUX', '2013-04-02' ),
+( 4, 1, 'LALA', '2013-02-21' ),
+( 4, 2, 'BMEX', '2013-03-19' ),
+( 4, 3, 'GEPP', '2013-04-18' );
 
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Manufactura Esbelta', 2, 'DEACERO', '2013-04-30', '' ), 
-( 'Manufactura Esbelta', 2, 'MABE', '2013-05-21', '' ), 
-( 'Manufactura Esbelta', 2, 'XIGNUX', '2013-06-18', '' );
+INSERT INTO INFRA.SESIONES ( ID_DIPLOMADO, SESION, EXPOSITOR,FECHA ) VALUES 
+( 5, 1, 'BIMBO', '2013-04-09' ),
+( 5, 2, 'FEMSA', '2013-05-07' ),
+( 5, 3, 'KCM', '2013-06-04' ),
+( 6, 1, 'CEMEX', '2013-04-23' ),
+( 6, 2, 'SLR', '2013-05-14' ),
+( 6, 3, 'PROEZA', '2013-06-11' ),
+( 7, 1, 'DEACERO', '2013-04-30' ),
+( 7, 2, 'MABE', '2013-05-21' ),
+( 7, 3, 'XIGNUX', '2013-06-18' ),
+( 8, 1, 'LALA', '2013-05-30' ),
+( 8, 2, 'BMEX', '2013-06-25' ),
+( 8, 3, 'GEPP', '2013-07-23' );
 
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Manufactura Esbelta', 3, 'DEACERO', '2013-07-18', '' ), 
-( 'Manufactura Esbelta', 3, 'MABE', '2013-08-15', '' ), 
-( 'Manufactura Esbelta', 3, 'XIGNUX', '2013-09-12', '' );
+INSERT INTO INFRA.SESIONES ( ID_DIPLOMADO, SESION, EXPOSITOR,FECHA ) VALUES 
+( 9, 1, 'BIMBO', '2013-07-02' ),
+( 9, 2, 'FEMSA', '2013-07-31' ),
+( 9, 3, 'KCM', '2013-08-20' ),
+( 10, 1, 'CEMEX', '2013-07-09' ),
+( 10, 2, 'SLR', '2013-08-08' ),
+( 10, 3, 'PROEZA', '2013-08-27' ),
+( 11, 1, 'DEACERO', '2013-07-18' ),
+( 11, 2, 'MABE', '2013-08-15' ),
+( 11, 3, 'XIGNUX', '2013-09-12' ),
+( 12, 1, 'LALA', null ),
+( 12, 2, 'BMEX', null ),
+( 12, 3, 'GEPP', null );
 
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Manufactura Esbelta', 4, 'DEACERO', '2013-09-17', '' ), 
-( 'Manufactura Esbelta', 4, 'MABE', '2013-10-15', '' ), 
-( 'Manufactura Esbelta', 4, 'XIGNUX', '2013-11-12', '' );
-
-
---Estrategia Comercial, Imágen y Cadena de Distribución
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 1, 'LALA', '2013-02-21', '' ), 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 1, 'BMEX', '2013-03-19', '' ), 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 1, 'GEPP', '2013-04-18', '' );
-
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 2, 'LALA', '2013-05-30', '' ), 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 2, 'BMEX', '2013-06-25', '' ), 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 2, 'GEPP', '2013-07-23', '' );
-
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 3, 'LALA', null, '' ), 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 3, 'BMEX', null, '' ), 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 3, 'GEPP', null, '' );
-
-INSERT INTO INFRA.DIPLOMADOS ( TEMA, GENERACION, UBICACION, FECHA, URL ) VALUES 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 4, 'LALA', '2013-09-26', '' ), 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 4, 'BMEX', '2013-10-24', '' ), 
-( 'Estrategia Comercial, Imágen y Cadena de Distribución', 4, 'GEPP', '2013-11-19', '' );
+INSERT INTO INFRA.SESIONES ( ID_DIPLOMADO, SESION, EXPOSITOR,FECHA ) VALUES 
+( 13, 1, 'BIMBO', '2013-09-04' ),
+( 13, 2, 'FEMSA', '2013-10-01' ),
+( 13, 3, 'KCM', '2013-10-29' ),
+( 14, 1, 'CEMEX', '2013-09-10' ),
+( 14, 2, 'SLR', '2013-10-08' ),
+( 14, 3, 'PROEZA', '2013-11-05' ),
+( 15, 1, 'DEACERO', '2013-09-17' ),
+( 15, 2, 'MABE', '2013-10-15' ),
+( 15, 3, 'XIGNUX', '2013-11-12' ),
+( 16, 1, 'LALA', '2013-09-26' ),
+( 16, 2, 'BMEX', '2013-10-24' ),
+( 16, 3, 'GEPP', '2013-11-19' );
