187be3d4953681711035f1be5c3ca7b047edb8a0
getsemani.correa <getsemani.correa@gmail.com>
2013-08-07 03:48:38 +0000

    actualizaciones varias

--- branches/vinculacion/src/mx/com/vgati/ccmx/vinculacion/ccmx/dao/CCMXDao.java
+++ branches/vinculacion/src/mx/com/vgati/ccmx/vinculacion/ccmx/dao/CCMXDao.java
@@ -23,17 +23,14 @@ public interface CCMXDao {
 
 	public List<Tractoras> getTractoras() throws DaoException;
 
-	public Mensaje saveUsuarioTractora(Tractoras tractoras) 
-			throws DaoException;
+	public Mensaje saveUsuarioTractora(Tractoras tractoras) throws DaoException;
 
-	public Mensaje saveRolTractora(Tractoras tractoras) 
-			throws DaoException;
+	public Mensaje saveRolTractora(Tractoras tractoras) throws DaoException;
 
-	public Mensaje saveTractora(Tractoras tractoras) 
-			throws DaoException;
+	public Mensaje saveTractora(Tractoras tractoras) throws DaoException;
 
-	public Mensaje updateTractora(Tractoras tractoras, String credenciales)
-			throws DaoException;
+	public Mensaje updateTractora(Tractoras tractoras, String credenciales,
+			String rol) throws DaoException;
 
 	public List<PyMEs> getPyMEs() throws DaoException;
 
@@ -43,8 +40,7 @@ public interface CCMXDao {
 
 	public Mensaje savePyME(PyMEs pyMEs) throws DaoException;
 
-	public List<Consultoras> getConsultoras(int id) 
-			throws DaoException;
+	public List<Consultoras> getConsultoras(int id) throws DaoException;
 
 	public Mensaje saveUsuarioConsultora(Consultoras consultoras)
 			throws DaoException;
@@ -52,32 +48,33 @@ public interface CCMXDao {
 	public Mensaje saveRolConsultora(Consultoras consultoras)
 			throws DaoException;
 
-	public Mensaje saveConsultora(Consultoras consultoras) 
-			throws DaoException;
+	public Mensaje saveConsultora(Consultoras consultoras) throws DaoException;
 
 	public Mensaje updateConsultora(Consultoras consultoras, String credenciales)
 			throws DaoException;
 
 	public Mensaje saveRelPyMETractora(PyMEs pyMEs) throws DaoException;
 
-	public String getNombreTractoras(int id)throws DaoException;
+	public String getNombreTractoras(int id) throws DaoException;
 
 	public Mensaje saveClientes(String nomTractora, int idPyME)
 			throws DaoException;
 
-	public Mensaje deshabilitaPyMEs(int estatus, boolean libera) throws DaoException;
-	
+	public Mensaje deshabilitaPyMEs(int estatus, boolean libera)
+			throws DaoException;
+
 	public List<Tractoras> getDetallesTractoras() throws DaoException;
 
-	public Mensaje saveDiplomados(Diplomados diplomado, int generacion) throws DaoException;
+	public Mensaje saveDiplomados(Diplomados diplomado, int generacion)
+			throws DaoException;
 
 	public Mensaje updateDiplomado(int id, String tema) throws DaoException;
 
-	public String getIdServicios(int id)throws DaoException;
+	public String getIdServicios(int id) throws DaoException;
 
-	public List<Integer> getListaIds(int id)throws DaoException;
+	public List<Integer> getListaIds(int id) throws DaoException;
 
-	public Mensaje deleteDomicilios(int id)throws DaoException;
+	public Mensaje deleteDomicilios(int id) throws DaoException;
 
 	public Mensaje deleteSesiones(int id) throws DaoException;
 
